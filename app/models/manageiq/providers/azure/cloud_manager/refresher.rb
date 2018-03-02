b87b2e4a5850f14e30eb50554da047b9f9a4aa72
Adam Grare <agrare@redhat.com>
2018-03-02 08:55:39 -0500

    Merge pull request #216 from tumido/consolidate_azure_refresh_workers
    Consolidate Azure refresh workers
    (cherry picked from commit 397527e7ec3ff0301749ab7bcebe9770bd615577)
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1558078

--- app/models/manageiq/providers/azure/cloud_manager/refresher.rb
+++ app/models/manageiq/providers/azure/cloud_manager/refresher.rb
@@ -53,6 +53,22 @@ module ManageIQ::Providers::Azure
       EmsRefresh.queue_refresh(ems.network_manager)
     end
 
+    def preprocess_targets
+      # sort the EMSes to be refreshed with cloud managers before other EMSes.
+      # since @targets_by_ems_id is a hash, we have to insert the items into a new
+      # hash in the order we want them to appear.
+      sorted_ems_targets = {}
+      # pull out the IDs of cloud managers and reinsert them in a new hash first, to take advantage of preserved insertion order
+      cloud_manager_ids = @targets_by_ems_id.keys.select { |key| @ems_by_ems_id[key].kind_of? ManageIQ::Providers::Azure::CloudManager }
+      cloud_manager_ids.each { |ems_id| sorted_ems_targets[ems_id] = @targets_by_ems_id.delete(ems_id) }
+      # now that the cloud managers have been removed from @targets_by_ems_id, move the rest of the values
+      # over to the new hash and then replace @targets_by_ems_id.
+      @targets_by_ems_id.keys.each { |ems_id| sorted_ems_targets[ems_id] = @targets_by_ems_id.delete(ems_id) }
+      @targets_by_ems_id = sorted_ems_targets
+
+      super
+    end
+
     def post_process_refresh_classes
       [::Vm]
     end
