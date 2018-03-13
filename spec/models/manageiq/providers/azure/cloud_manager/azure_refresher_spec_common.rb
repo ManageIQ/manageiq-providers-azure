949197815654ce8c926c8476511cb69ee3fc645d
Ladislav Smola <lsmola@redhat.com>
2018-03-13 17:22:03 +0100

    Test less combinations to avoid CI limit
    Test less combinations to avoid CI limit

--- spec/models/manageiq/providers/azure/cloud_manager/azure_refresher_spec_common.rb
+++ spec/models/manageiq/providers/azure/cloud_manager/azure_refresher_spec_common.rb
@@ -13,19 +13,8 @@ module AzureRefresherSpecCommon
       :inventory_object_refresh => true,
       :inventory_collections    => {
         :saver_strategy => :batch,
-        :use_ar_object  => true,
-      },
-    }, {
-      :get_private_images       => true,
-      :inventory_object_refresh => true,
-      :inventory_collections    => {
-        :saver_strategy => :batch,
         :use_ar_object  => false,
       },
-    }, {
-      :get_private_images               => true,
-      :inventory_object_saving_strategy => :recursive,
-      :inventory_object_refresh         => true
     }
   ].freeze
 
