5722bec4da29e9df74333a711253849a7d6f0c41
Milan Zazrivec <mzazrivec@redhat.com>
2019-01-08 09:57:07 +0100

    Add display name for Floating IP

--- app/models/manageiq/providers/azure/network_manager/floating_ip.rb
+++ app/models/manageiq/providers/azure/network_manager/floating_ip.rb
@@ -2,4 +2,8 @@ class ManageIQ::Providers::Azure::NetworkManager::FloatingIp < ::FloatingIp
   def self.available
     joins(:network_port).where("network_ports.device_id" => nil) + where(:network_port_id => nil)
   end
+
+  def self.display_name(number = 1)
+    n_('Floating IP (Microsoft Azure)', 'Floating IPs (Microsoft Azure)', number)
+  end
 end
