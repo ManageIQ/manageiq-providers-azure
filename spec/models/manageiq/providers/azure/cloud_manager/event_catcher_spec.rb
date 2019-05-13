461578f4fff485d1e78e9b77789975e653f2daea
Keenan Brock <keenan@thebrocks.net>
2019-05-13 11:32:06 -0400

    remove stub of all_ems_in_zone

--- spec/models/manageiq/providers/azure/cloud_manager/event_catcher_spec.rb
+++ spec/models/manageiq/providers/azure/cloud_manager/event_catcher_spec.rb
@@ -5,18 +5,17 @@ describe ManageIQ::Providers::Azure::CloudManager::EventCatcher do
     before do
       server = EvmSpecHelper.local_miq_server
       @ems = FactoryGirl.create(:ems_azure, :with_authentication, :zone => server.zone)
-      allow(described_class).to receive(:all_ems_in_zone).and_return([@ems])
     end
 
     it "returns a valid ems for zone if timeline events are supported" do
-      allow(@ems).to receive(:supports_timeline?).and_return(true)
+      expect_any_instance_of(@ems.class).to receive(:supports_timeline?).and_return(true)
       expect($log).not_to receive(:info).with(/#{unsupported_reason}/)
       expect(described_class.all_valid_ems_in_zone).to include(@ems)
     end
 
     it "returns an empty list for zone if timeline events are not supported" do
-      allow(@ems).to receive(:supports_timeline?).and_return(false)
-      allow(@ems).to receive(:unsupported_reason).and_return(unsupported_reason)
+      expect_any_instance_of(@ems.class).to receive(:supports_timeline?).and_return(false)
+      expect_any_instance_of(@ems.class).to receive(:unsupported_reason).and_return(unsupported_reason)
       expect($log).to receive(:info).with(/#{unsupported_reason}/)
       expect(described_class.all_valid_ems_in_zone).not_to include(@ems)
     end
