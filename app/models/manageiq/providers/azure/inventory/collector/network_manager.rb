e668df79e81e51e36c78ecd204393d64caa9a0b3
Jason Frey <fryguy9@gmail.com>
2021-04-09 17:23:33 -0400

    Merge pull request #454 from agrare/combined_manager_refresh
    Refresh Cloud and Network managers together
    
    (cherry picked from commit 62687cf475909c101b840135181053b955026a6c)

--- app/models/manageiq/providers/azure/inventory/collector/network_manager.rb
+++ app/models/manageiq/providers/azure/inventory/collector/network_manager.rb
@@ -1,17 +1,2 @@
 class ManageIQ::Providers::Azure::Inventory::Collector::NetworkManager < ManageIQ::Providers::Azure::Inventory::Collector
-  def cloud_networks
-    gather_data_for_this_region(@vns)
-  end
-
-  def security_groups
-    gather_data_for_this_region(@nsg)
-  end
-
-  def load_balancers
-    @load_balancers ||= gather_data_for_this_region(@lbs)
-  end
-
-  def network_routers
-    @network_routers = gather_data_for_this_region(@rts)
-  end
 end
