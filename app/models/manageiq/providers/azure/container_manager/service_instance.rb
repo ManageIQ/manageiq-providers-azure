6dbfd3c1ed7f6b80498cea9a72cf798e501a6aeb
Adam Grare <adam@grare.com>
2022-01-10 10:31:44 -0500

    Add missing container_manager subclasses

---
+++ app/models/manageiq/providers/azure/container_manager/service_instance.rb
@@ -0,0 +1,4 @@
+ManageIQ::Providers::Kubernetes::ContainerManager::ServiceInstance.include(ActsAsStiLeafClass)
+
+class ManageIQ::Providers::Azure::ContainerManager::ServiceInstance < ManageIQ::Providers::Kubernetes::ContainerManager::ServiceInstance
+end
