973efaf73dbe8176b4790d89bcc0ca392d9d908f
Samuel Liu <liusamuelt@gmail.com>
2024-09-04 15:00:15 -0400

    Clean up network interface and public ip on vm provision failure

--- app/models/manageiq/providers/azure/cloud_manager/provision/state_machine.rb
+++ app/models/manageiq/providers/azure/cloud_manager/provision/state_machine.rb
@@ -6,4 +6,24 @@ module ManageIQ::Providers::Azure::CloudManager::Provision::StateMachine
 
     signal :post_create_destination
   end
+
+  def clone_failure_cleanup
+    return if phase_context[:clone_options].nil?
+    
+    delete_instance(phase_context[:clone_options]) if phase_context[:clone_options]
+    signal :provision_error
+  rescue Azure::Armrest::BadRequestException
+    requeue_phase(3.minutes)
+  end
+
+  def delete_instance(instance_attrs)
+    source.with_provider_connection do |azure|
+      nis = ::Azure::Armrest::Network::NetworkInterfaceService.new(azure)
+      ips = ::Azure::Armrest::Network::IpAddressService.new(azure)
+      ni = nis.get(instance_attrs[:name], resource_group.name)
+      ip = ips.get("#{instance_attrs[:name]}-publicIp", resource_group.name)
+      nis.delete(instance_attrs[:name], resource_group.name) if ni
+      ips.delete("#{instance_attrs[:name]}-publicIp", resource_group.name) if ip
+    end
+  end
 end
--- spec/factories/image.rb
+++ spec/factories/image.rb
@@ -1,3 +1,7 @@
 FactoryBot.define do
-  factory :azure_image, :class => 'ManageIQ::Providers::Azure::CloudManager::Template'
+  factory :azure_image, :class => 'ManageIQ::Providers::Azure::CloudManager::Template' do
+    sequence(:name) { |n| "Azure Image #{n}" }
+    location { "azure" }
+    vendor { "azure" }
+  end
 end
